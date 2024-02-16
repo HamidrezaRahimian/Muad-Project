4bcac62c4446dcf5fd4831575430a3528b59ec70
SaskiaStier <sassika25@gmail.com>
2024-02-16 14:23:49 +0100

    adding date

--- impressium.php
+++ impressium.php
@@ -50,27 +50,27 @@ stiersaskia6@gmail.com <br>
 +4915753730107
 
 <h3>Haftungsausschluss:</h3>
-Die bereitgestellten Inhalte auf dieser Website dienen ausschließlich Unterhaltungszwecken. <br>
-Wir übernehmen keine Gewähr für die Vollständigkeit, Richtigkeit, Verlässlichkeit, Eignung oder Verfügbarkeit der Informationen, <br>
-Produkte, Dienstleistungen oder zugehörigen Grafiken auf der Website für irgendeinen Zweck. <br>
-Jegliches Vertrauen, das du in solche Informationen setzt, geschieht daher auf eigene Gefahr. <br>
+Die bereitgestellten Inhalte auf dieser Website dienen ausschließlich Unterhaltungszwecken. 
+Wir übernehmen keine Gewähr für die Vollständigkeit, Richtigkeit, Verlässlichkeit, Eignung oder Verfügbarkeit der Informationen, 
+Produkte, Dienstleistungen oder zugehörigen Grafiken auf der Website für irgendeinen Zweck. 
+Jegliches Vertrauen, das du in solche Informationen setzt, geschieht daher auf eigene Gefahr. 
 
 <h3>Geistiges Eigentum:</h3>
-Alle geistigen Eigentumsrechte für die Inhalte und Materialien auf dieser Website, einschließlich, aber nicht beschränkt auf Texte, <br>
-Grafiken, Logos, Bilder, Audio-Clips, digitale Downloads und Software, sind im Besitz von Moritz, Alex und Saskia <br>
-oder der jeweiligen Eigentümer und werden durch geltende Urheber- und Markengesetze geschützt. <br>
+Alle geistigen Eigentumsrechte für die Inhalte und Materialien auf dieser Website, einschließlich, aber nicht beschränkt auf Texte, 
+Grafiken, Logos, Bilder, Audio-Clips, digitale Downloads und Software, sind im Besitz von Moritz, Alex und Saskia 
+oder der jeweiligen Eigentümer und werden durch geltende Urheber- und Markengesetze geschützt. 
 
 <h3>Externe Links:</h3>
-Diese Website kann Links zu externen Websites enthalten, die nicht von uns bereitgestellt oder gewartet werden. <br>
-Wir garantieren nicht für die Genauigkeit, Relevanz, Aktualität oder Vollständigkeit der Informationen auf diesen externen Websites. <br>
+Diese Website kann Links zu externen Websites enthalten, die nicht von uns bereitgestellt oder gewartet werden. 
+Wir garantieren nicht für die Genauigkeit, Relevanz, Aktualität oder Vollständigkeit der Informationen auf diesen externen Websites. 
 Die Aufnahme von externen Links bedeutet nicht zwangsläufig eine Empfehlung oder Unterstützung der darin zum Ausdruck gebrachten Ansichten.
 
 <h3>Änderungen des Impressums:</h3>
-Wir behalten uns das Recht vor, dieses Impressum jederzeit ohne vorherige Ankündigung zu aktualisieren oder zu ändern. <br>
+Wir behalten uns das Recht vor, dieses Impressum jederzeit ohne vorherige Ankündigung zu aktualisieren oder zu ändern. 
 Es liegt in deiner Verantwortung, diese Seite regelmäßig auf Änderungen zu überprüfen.
 
 <h3>Zuletzt aktualisiert:</h3>
- [Datum einfügen]
+ 16.02.2024
   <br><br><br>
 
 
