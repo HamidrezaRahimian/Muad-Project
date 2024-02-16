9c45489697acc0c00c9247bb72adf9575347a22a
Moritz Ungemach <Moritz.Ungemach@flyeralarm.com>
2024-02-16 13:17:11 +0100

    form hinzugefügt

---
+++ form.php
@@ -0,0 +1,89 @@
+<!doctype html>
+<html lang="de" xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Gästebuch</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+<header>
+    <?php include "header.html" ?>
+</header>
+
+<nav>
+    <?php include "nav.html" ?>
+</nav>
+
+<section>
+    <div class="flex-container">
+
+
+                <form method="post" action="form.php">
+                        Name:
+                        <label>
+                            <input type="text" name="name">
+                        </label>
+                        <br>
+                        Besucht am:
+                        <label>
+                            <input type="date" name="datum">
+                        </label>
+                        <br>
+                        Feedback:
+                        <label>
+                            <input type="text" name="eintrag">
+                        </label>
+                        <br>
+                        <input type="submit" value="Eintragen">
+                </form>
+    </div>
+        <?php
+            if ($_SERVER["REQUEST_METHOD"] === "POST") {
+                $schon_drin = "";
+                $daten = "nix";
+                $name = htmlspecialchars($_POST['name']);
+                $datum = htmlspecialchars($_POST['datum']);
+                $eintrag = htmlspecialchars($_POST['eintrag']);
+                $user_info = array($name, $datum, $eintrag);
+                if (!empty($name) and !empty($datum) and !empty($eintrag)) {
+                    $daten = implode(";", $user_info) . "\r\n";
+                    $zitate = file("Datei");
+                    for ($i = 0; $i < count($zitate); $i++) {
+                        if ($daten != $zitate[$i]) {
+                            $schon_drin = false;
+                        } else {
+                            $schon_drin = true;
+                        }
+
+                    }
+                    if (!$schon_drin) {
+                        file_put_contents("Datei", $daten, FILE_APPEND);
+                    } else {
+                        echo "Bitte alle Felder ausfüllen" . "<br><br>";
+                    }
+                }
+            }
+        ?>
+    <Strong>Einträge</Strong>
+        <div class="flex-container">
+            <?php
+            $zitate = file("Datei");
+            for ($i = 0; $i < count($zitate); $i++) {
+                echo $i+1 . ": " . $zitate[$i] . "<br><br>";
+
+        }
+
+
+        ?>
+        </div>
+</section>
+
+<footer>
+        <?php include "footer.html" ?>
+</footer>
+
+</body>
+</html>
