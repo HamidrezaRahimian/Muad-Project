37c55e0f389170862748b55444de2de47060415b
SaskiaStier <sassika25@gmail.com>
2024-02-16 15:12:40 +0100

    gi

--- impressium.php
+++ impressium.php
@@ -24,6 +24,7 @@
 
   <h2>Impressum</h2>  
 
+
 Website-Name: [Website-Name einfügen] <br>
 Website-URL: [Website-URL einfügen] <br>
 
@@ -54,34 +55,27 @@ Die bereitgestellten Inhalte auf dieser Website dienen ausschließlich Unterhalt
 Wir übernehmen keine Gewähr für die Vollständigkeit, Richtigkeit, Verlässlichkeit, Eignung oder Verfügbarkeit der Informationen, 
 Produkte, Dienstleistungen oder zugehörigen Grafiken auf der Website für irgendeinen Zweck. 
 Jegliches Vertrauen, das du in solche Informationen setzt, geschieht daher auf eigene Gefahr. 
-
+<hr>
 <h3>Geistiges Eigentum:</h3>
 Alle geistigen Eigentumsrechte für die Inhalte und Materialien auf dieser Website, einschließlich, aber nicht beschränkt auf Texte, 
 Grafiken, Logos, Bilder, Audio-Clips, digitale Downloads und Software, sind im Besitz von Moritz, Alex und Saskia 
 oder der jeweiligen Eigentümer und werden durch geltende Urheber- und Markengesetze geschützt. 
-
+<hr>
 <h3>Externe Links:</h3>
 Diese Website kann Links zu externen Websites enthalten, die nicht von uns bereitgestellt oder gewartet werden. 
 Wir garantieren nicht für die Genauigkeit, Relevanz, Aktualität oder Vollständigkeit der Informationen auf diesen externen Websites. 
 Die Aufnahme von externen Links bedeutet nicht zwangsläufig eine Empfehlung oder Unterstützung der darin zum Ausdruck gebrachten Ansichten.
-
+<hr>
 <h3>Änderungen des Impressums:</h3>
 Wir behalten uns das Recht vor, dieses Impressum jederzeit ohne vorherige Ankündigung zu aktualisieren oder zu ändern. 
 Es liegt in deiner Verantwortung, diese Seite regelmäßig auf Änderungen zu überprüfen.
-
+<hr>
 <h3>Zuletzt aktualisiert:</h3>
  16.02.2024
-  <br><br><br>
-
-
-
-  
-        <a href="index.php">Startseite</a>
+          <a href="index.php">Startseite</a>
     </section>
     <footer>
         <?php include "footer.html" ?>
     </footer>
-
 </body>
-
 </html>
