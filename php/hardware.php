4a725f695ba3762f53c3f6b08ad9797084140db7
Alex <hamidreza.rahimian@emerson.com>
2024-03-07 08:24:29 +0100

    umforming alles

---
+++ php/hardware.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Digitalisierung</title>
+    <link rel="stylesheet" href="index.css">
+    <link rel="icon" type="image/png" href="img/favicon.png">
+</head>
+
+<body>
+
+    <header>
+        <?php include "header.html" ?>
+    </header>
+    <nav>
+        <?php include "nav.html" ?>
+    </nav>
+    <section>
+        <h2>Hardware
+        </h2>
+        <ul class="listofdata">
+            <li class="eachlist"><strong>Hauptplatine:</strong>
+                <p class="textoflist">
+                    Die Hauptplatine, oft auch als Motherboard bezeichnet, ist das zentrale Nervensystem eines Computers.
+                    Sie fungiert als Verbindungszentrum für alle Komponenten des Systems. Auf der Hauptplatine sind der
+                    Prozessor (CPU), der Arbeitsspeicher (RAM), Steckplätze für Erweiterungskarten, Anschlüsse für
+                    Festplatten und weitere Peripheriegeräte fest verankert. Durch den Einsatz von Bussen und Schaltkreisen
+                    koordiniert die Hauptplatine den reibungslosen Datenverkehr zwischen den verschiedenen
+                    Hardwarekomponenten, was sie zu einer essenziellen Schaltzentrale für die Computerarchitektur macht.
+                </p>
+
+            </li>
+            <li class="eachlist"><strong>Eingabe:</strong>
+                <p class="textoflist">
+                    Eingabegeräte dienen dazu, Informationen in den Computer einzuführen, und die Hauptplatine
+                    spielt hier eine entscheidende Rolle. Tastaturen, Mäuse, Grafiktabletts und andere Eingabegeräte
+                    sind über entsprechende Anschlüsse direkt oder drahtlos mit der Hauptplatine verbunden. Die
+                    Hauptplatine interpretiert die von diesen Geräten generierten Signale und leitet sie an den
+                    Prozessor weiter. Somit ermöglicht die Hauptplatine die Interaktion des Benutzers mit dem
+                    Computer und die Eingabe von Daten für weitere Verarbeitungsschritte.
+                </p>
+            </li>
+
+            <li class="eachlist"><strong>Ausgabe:</strong>
+                <p class="textoflist">
+                    Ebenso wichtig ist die Ausgabe von Informationen, und auch hier spielt die Hauptplatine
+                    eine Schlüsselrolle. Monitore, Drucker, Lautsprecher und andere Ausgabegeräte werden
+                    über die entsprechenden Anschlüsse an die Hauptplatine angeschlossen. Sie empfängt die
+                    Daten vom Prozessor, interpretiert sie und leitet sie an die Ausgabegeräte weiter.
+                    Dadurch ermöglicht die Hauptplatine die Darstellung von visuellen, auditiven oder
+                    gedruckten Informationen, die das Ergebnis der Computerberechnungen und -prozesse sind.
+                </p>
+            </li>
+            <li class="eachlist"><strong>Externspeicher:</strong>
+                <p class="textoflist">
+                    Externer Speicher, wie Festplatten und USB-Laufwerke, erweitert die
+                    Speicherkapazität eines Computers über die auf der Hauptplatine integrierten
+                    Speicheroptionen hinaus. Die Hauptplatine verfügt über Schnittstellen, wie SATA
+                    - oder PCIe-Steckplätze, um externe Speichergeräte anzuschließen. Diese externen
+                    Speichermedien ermöglichen die sichere Aufbewahrung von Daten und Anwendungen
+                    , die nicht nur die interne Festplatte nutzen. Die Hauptplatine fungiert als
+                    Mittler zwischen dem externen Speicher und dem Rest des Systems, indem sie
+                    den Datenaustausch über die entsprechenden Schnittstellen ermöglicht.
+                </p>
+            </li>
+
+        </ul>
+        <a href="index.php">Startseite</a>
+    </section>
+
+    <footer>
+        <?php include "footer.html" ?>
+    </footer>
+
+</body>
+
+</html>
