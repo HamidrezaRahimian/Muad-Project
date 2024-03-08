d26a61979c640d87f248bfdbdfc84f62d665d29f
Moritz Ungemach <Moritz.Ungemach@flyeralarm.com>
2024-03-08 12:54:34 +0100

    bug fix

--- php/form.php
+++ php/form.php
@@ -54,7 +54,7 @@
             $user_info = array($name, $datum, $eintrag, $art);
             if (!empty($name) and !empty($datum) and !empty($eintrag) and !empty($art)) {
                 $daten = implode(";", $user_info) . "\r\n";
-                $zitate = file("Datei");
+                $zitate = file("../assets/Data/KontactData");
                 for ($i = 0; $i < count($zitate); $i++) {
                     if ($daten != $zitate[$i]) {
                         $schon_drin = false;
@@ -63,7 +63,7 @@
                     }
                 }
                 if (!$schon_drin) {
-                    file_put_contents("Datei", $daten, FILE_APPEND);
+                    file_put_contents("../assets/Data/KontactData", $daten, FILE_APPEND);
                 } else {
                     echo "Bitte alle Felder ausfüllen" . "<br><br>";
                 }
@@ -76,7 +76,7 @@
             <details>
                 <summary><Strong>Einträge</Strong></summary>
                 <p><?php
-                    $zitate = file("Datei");
+                    $zitate = file("../assets/Data/KontactData");
                     for ($i = 0; $i < count($zitate); $i++) {
                         echo $i + 1 . ": " . $zitate[$i] . "<br><br>";
                     }
