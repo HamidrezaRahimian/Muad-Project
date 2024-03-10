afc0194dc4c4a532ac6830a2c458605862827264
Moritz Ungemach <Moritz.Ungemach@flyeralarm.com>
2024-03-10 12:04:05 +0100

    kontakt feedback fix

--- php/feedback.php
+++ php/feedback.php
@@ -27,13 +27,13 @@
                 <input type="date" name="datum" id="datum" class="form-input" autocomplete="off">
 
                 <label for="art" class="form-label">Art: *</label>
-                <select name="art" id="art" class="form-input" autocomplete="off">
+                <select name="art" id="art" class="form-input" >
                     <option value="Feedback">Feedback</option>
                     <option value="Sonstiges">Sonstiges</option>
                 </select>
 
                 <label for="nachricht" class="form-label">Text: *</label>
-                <textarea name="eintrag" id="nachricht" class="form-textarea" cols="30" rows="10" autocomplete="off"></textarea>
+                <textarea name="eintrag" id="nachricht" class="form-textarea" cols="30" rows="10" ></textarea>
 
                 <button type="submit" class="form-button">Absenden</button>
             </form>
@@ -55,7 +55,7 @@
             $user_info = array($name, $datum, $eintrag, $art);
             if (!empty($name) and !empty($datum) and !empty($eintrag) and !empty($art)) {
                 $daten = implode(";", $user_info) . "\r\n";
-                $zitate = file("../assets/data/feedback-data");
+                $zitate = file("../assets/data/feedbackdata");
                 for ($i = 0; $i < count($zitate); $i++) {
                     if ($daten != $zitate[$i]) {
                         $schon_drin = false;
@@ -64,7 +64,7 @@
                     }
                 }
                 if (!$schon_drin) {
-                    file_put_contents("../assets/data/feedback-data", $daten, FILE_APPEND);
+                    file_put_contents("../assets/data/feedbackdata", $daten, FILE_APPEND);
                 } else {
                     echo "Bitte alle Felder ausfüllen" . "<br><br>";
                 }
@@ -77,7 +77,7 @@
             <details>
                 <summary><Strong>Einträge</Strong></summary>
                 <p><?php
-                    $zitate = file("../assets/data/feedback-data");
+                    $zitate = file("../assets/data/feedbackdata");
 
                     for ($i = 0; $i < count($zitate); $i++) {
                         echo $i + 1 . ": " . $zitate[$i] . "<br><br>";
